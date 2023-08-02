1b2965a519f7839d041351f8852bfec9b17f6ab5
Rohit <rohityadav787898@gmail.com>
2023-08-02 14:28:36 +0545

    managing components folder and learning hooks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Title from "./components/Title";
 import ButtonComponent from "./components/Button";
 import Home from "./components/Nav";
-import Goal from "./components/Goal";
-import Garage from "./components/Garage";
+import Garage from "./learning/Garage";
+import FavoriteColor from "./learning/FavoriteColor";
 function App() {
   return (
     <>
       <Home />
+      <Title />
       <ButtonComponent />
       <Garage />
+      <FavoriteColor />
     </>
   );
 }
---
+++ client/src/learning/FavoriteColor.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+export default function FavoriteColor() {
+  const [color, setColor] = useState("red");
+
+  return (
+    <>
+      <h1>My favorite color is {color}!</h1>
+      <button type="button" onClick={() => setColor("blue")}>
+        Blue
+      </button>
+      <button type="button" onClick={() => setColor("red")}>
+        Red
+      </button>
+    </>
+  );
+}
